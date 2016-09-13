c05b782bf7dffdc55019eecd03df049a87581223
Andy Stoica <andy.stoica@me.com>
2016-09-13 13:15:46 +0100

    TShirt colors behaviour

--- js/form.js
+++ js/form.js
@@ -1,6 +1,18 @@
+/**
+ * HTML snippets
+ */
+
+var colorsJsPuns  = '<option value="cornflowerblue">Cornflower Blue</option>' +
+                    '<option value="darkslategrey">Dark Slate Grey</option>' +
+                    '<option value="gold">Gold</option>';
+var colorsHeartJs = '<option value="tomato">Tomato</option>' +
+                    '<option value="steelblue">Steel Blue</option>' +
+                    '<option value="dimgrey">Dim Grey</option>';
+
+
 
 /**
- * Main form behaviour
+ * Enhanced form behaviour
  */
 
 // Focus on the first field when the page loads
@@ -16,5 +28,27 @@ $('#title').change(function(){
     $('#other-title').show();
   } else {
     $('#other-title').hide();
+  }
+});
+
+// Hide the color section on page load
+$('#colors-js-puns').hide();
+
+// Show the correct color selection sub menu when the user
+// selects a new theme or remove the color selection of no
+// theme is chosen
+$('#design').change(function(){
+  switch($(this).val().toLowerCase()) {
+    case 'js puns':
+      $('#color').html(colorsJsPuns);
+      $('#colors-js-puns').show();
+      break;
+    case 'heart js':
+      $('#color').html(colorsHeartJs);
+      $('#colors-js-puns').show();
+      break;
+    default:
+      $('#color').html('');
+      $('#colors-js-puns').hide();
   }
 });
